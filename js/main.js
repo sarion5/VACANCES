67b8b3c5ce72d46faed7c74594bd46a27e31449b
sarion5 <sarion5@users.noreply.github.com>
2018-07-26 12:14:29 +0200

    separate creation and shuffle of deck

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 
 /**
  * initPlayers
+ * 
  * create players that will play de game using nbPlayers
  * and create names accordingly
  * @return {array} players[] with all players involved
@@ -24,6 +25,72 @@ function initPlayers() {
     return players;
 }
 
+/**
+ * initDeck
+ * 
+ * create a deck by pushing all trait cards into an aray
+ * 
+ * @return {Array} deck with all trait cards
+ */
+function initDeck(AllTraitCards) {
+
+    // 1) initialize deck
+    let deck = [];
+
+    //add one trait card by copies this trait card has left
+    //e.g if there is 7 copies of the object "Ambush", there will be
+    //7 copies of "Ambush" in the deck
+    for (let j = 0; j < AllTraitCards.length; j++) {
+        for (let k = 0; k < AllTraitCards[j].nbCopies; k++) {
+            deck.push(AllTraitCards[j]);
+        }
+    }
+
+    return deck;
+}
+
+/**
+ * shuffleDeckAndDistribute
+ * 
+ * distribute random cards from the deck to the current player's hand
+ * and remove cards distributed from the deck
+ * @param {Array} AllTraitCards all trait cards that will be push into the deck
+ * @param {Object} currentPlayer player who's turn is currently running
+ * 
+ * @return {Array} deck minus cards distributed 
+ */
+function shuffleDeckAndDistribute(initDeck, currentPlayer) {
+
+    //create a deck
+    let deck = initDeck(AllTraitCards);
+
+    //the player receives 3 cards + one for each specie they own
+    //addition of the three initial cards and the +1 by specie owned by the player
+    let cardsDistrubution = currentPlayer.cardsInit + currentPlayer.species;
+
+    //shuffle and take off cards distributed
+    for (let j = 0; j < cardsDistrubution; j++) {
+        //shuffle
+        let currentCardNumber = Math.floor(Math.random() * Math.floor(deck.length));
+        let currentCard;
+        do {
+            currentCard = deck[currentCardNumber];
+            console.log(currentCard)
+        } while (currentCard.nbCopies < 1);
+
+
+        //take off from main deck
+        currentCard.nbCopies -= 1;
+
+        //put into currentPlayerHand
+        (currentPlayer.cardsInHand).push(currentCard);
+    }
+
+    deck = initDeck(AllTraitCards);
+
+    return deck;
+}
+
 //***************************************************************
 //***************************************************************
 //***************************************************************
@@ -33,6 +100,7 @@ function initPlayers() {
 var players = initPlayers();
 
 function roundPartOne(players, AllTraitCards) {
+
 
     //loop on every players
     for (let i = 0; i < players.length; i++) {
@@ -45,43 +113,10 @@ function roundPartOne(players, AllTraitCards) {
             currentPlayer.species = 1;
         }
 
-        function shuffleDeck(AllTraitCards, currentPlayer) {
+        // shuffle deck and distribute cards
+        console.log(shuffleDeckAndDistribute(initDeck, currentPlayer));
 
 
-            let deck = [];
-
-            //add one trait card by copies this trait card has left
-            //e.g if there is 7 copies of the object "Ambush", there will be
-            //7 copies of "Ambush" in the deck
-            for (let j = 0; j < AllTraitCards.length; j++) {
-                for (let k = 0; k < AllTraitCards[j].nbCopies; k++) {
-                    deck.push(AllTraitCards[j]);
-                }
-            }
-
-            //addition of the three initial cards and the +1 by specie owned by the player
-            let cardsDistrubution = currentPlayer.cardsInit + currentPlayer.species;
-
-            //shuffle and take off cards distributed using slice()
-            for (let j = 0; j < cardsDistrubution; j++) {
-
-                //shuffle
-                let currentCardNumber = Math.floor(Math.random() * Math.floor(deck.length));
-                let currentCard = deck[currentCardNumber];
-                console.log(currentCard);
-                (currentPlayer.cardsInHand).push(currentCard)
-
-                //take off
-                currentCard.nbCopies
-            }
-
-
-
-            return newDeck;
-        }
-
-
-        console.log(shuffleDeck(AllTraitCards, currentPlayer));
     }
 }
 
